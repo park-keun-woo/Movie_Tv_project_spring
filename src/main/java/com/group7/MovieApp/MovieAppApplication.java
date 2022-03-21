68fe7df0f488ad7303714a57f224df496240c2d5
yujay0529 <warmyeol0987@naver.com>
2022-03-21 15:23:49 +0900

    properties 추가

--- src/main/java/com/group7/MovieApp/MovieAppApplication.java
+++ src/main/java/com/group7/MovieApp/MovieAppApplication.java
@@ -1,9 +1,19 @@
 package com.group7.MovieApp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.multi.spring_boot_react.MainController;
+import com.multi.spring_boot_react.product.IProductDAO;
+import com.multi.spring_boot_react.product.ProductController;
 
 @SpringBootApplication
+
+@ComponentScan(basePackageClasses = MainController.class)
+@ComponentScan(basePackageClasses = ReactController.class)
+//@MapperScan(basePackageClasses = IProductDAO.class)
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
